354e31860fc39c1be738322d047faea43e1ff7a5
mismetti <milastrokes@gmail.com>
2021-09-09 18:46:16 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div>
+    <Fragment>
+      <NavBar/>
+    <div className="container">
       <h1 className="text-primary">aa</h1>
+     
     </div>
+    <Footer/>
+    <DataTable/>
+    </Fragment>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/mismetti" target="_blank" rel="noreferrer">Mila Mismetti</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Estudo React e Springboot<a href="https://instagram.com/undefinedwaifu" target="_blank" rel="noreferrer">@undefinedwaifu</a></small></p>
+  </div>
+</footer>
+    );
+  }
+  
+  export default Footer;
+  
